41883edde72d60cd65acb79becd5fb613cb648d7
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2021-02-23 22:26:39 +0600

    Add files via upload

--- pz1.py
+++ pz1.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+#1pz
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+duration = int(input("Enter all the time in second: "))
+
+days = duration // 3600 // 25
+hours = duration // 3600 - days * 24
+minutes = duration // 60 % 60
+seconds = duration % 60
+
+print("days: ", days, ",", "hours: ", hours, ",", "minutes: ", minutes, ",", "seconds: ", seconds, ".", sep="")
+
+#---------------------------------------------------------------------------------------------------------
+#2pz
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+list = []
+add_list = []
+sum = 0
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for i in range(1, 1001, 2):
+    list.append(i**3)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for ind, val in enumerate(list):
+    sum_digits = 0
+    while val > 0:
+        sum_digits += val % 10
+        val //= 10
+    if sum_digits % 7 == 0:
+        sum += add_list[ind]
+
+print(sum)
+
+for m in list:
+    add_list.append(m+17)
+
+sum = 0
+
+for ind, val in enumerate(add_list):
+    sum_digits = 0
+    while val > 0:
+        sum_digits += val % 10
+        val //= 10
+    if sum_digits % 7 == 0:
+        sum += add_list[ind]
+
+print(sum)
+
+
+
+
+#--------------------------------------------------------------------------------------------------
+#pz3
+
+for procent in range (20):
+    if procent % 10 == 1 and procent % 20 != 11:
+        print(procent, "процент", end=" ")
+    elif 1 < procent % 10 < 5 and not 11 < procent % 20 < 15:
+        print(procent, "процента", end=" ")
+    else:
+        print(procent, "процентов", end=" ")
